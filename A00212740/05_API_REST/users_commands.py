cf4492e3ffdfa09632adb1205c1eeae17d505f9c
Christian <crisdavcar@gmail.com>
2017-04-20 14:34:50 -0500

    Update users_commands.py

--- A00212740/05_API_REST/users_commands.py
+++ A00212740/05_API_REST/users_commands.py
@@ -28,7 +28,7 @@ def get_that_user(username):
 def recently_logged():
  grep = Popen(["lastlog","-t","5"],stdout=PIPE, stderr=PIPE)
  logged = Popen(["awk",'-F',':','{print $1}'],stdin=grep.stdout, stdout=PIPE, stderr=PIPE).communicate()[0].split('\n')
- return logged
+ return filter(None,logged)
 
 def recently_commands(username):
  if username in get_all_users():
